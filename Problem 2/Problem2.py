34985bc00a702950367943bf211bde50423c18eb
Thomas Wimprine <twimprine@gmail.com>
2024-09-29 21:41:20 -0500

    Working on P2 logic

---
+++ Problem 2/Problem2.py
@@ -0,0 +1,30 @@
+result = 0
+counter = 0
+endNumber = 10
+
+fibA = 1
+fibB = 1
+
+def getFib(fibA, fibB):
+    return fibA + fibB
+
+def nextFib():
+    global fibA, fibB
+    nextFib = getFib(fibA, fibB)
+    fibA = fibB  # Move fibA to fibB's position
+    fibB = nextFib  # Update fibB to the next Fibonacci value
+    return fibB
+
+while counter < endNumber:
+    newFib = nextFib()
+    print('FibA: ', fibA)
+    print('FibB: ', fibB)
+    print('Next Fib: ', newFib)
+    
+    if (newFib % 2) == 0:
+        result += newFib  # Add only even Fibonacci numbers
+    
+    print('Counter: ', counter)
+    counter += 1
+
+print('Solution: ', result)
